7f49ba6cc17b61ed22f8983cb9561102905f5cdc
Stas <stas3112rus@yandex.ru>
2022-03-20 16:56:02 +0300

    use Form from formik with context

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,4 @@
-import {useFormik} from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup'
 
 // const validate = values =>{
@@ -20,107 +20,91 @@ import * as Yup from 'yup'
 //     return errors;
 // }
 
-const Form = () => {
-
-    const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            amount: 0,
-            currency: '',
-            text: '',
-            terms: '',
-        },
-        // validate,
-        validationSchema: Yup.object({
-            name: Yup.string()
-                    .min(2, "Минимум 2 символа")
-                    .required('Поле обязательное'),
-            email: Yup.string()
-                    .email("введите корректный Email")
-                    .required('Поле обязательное'),
-            amount: Yup.number()
-                        .min(5, "Не менее 5")
-                        .required('Поле обязательное'),
-            currency: Yup.string().required('Выберете валюту'),
-            text: Yup.string().min(10, 'Не менее 10 символов'),
-            terms: Yup.boolean()
-                        .oneOf([true], 'Необходимо согласие')
-                        .required('Необходимо согласие')
-
-            
-        }), 
-        onSubmit: values => console.log(JSON.stringify(values, null, 2)),
-        
-    })
+const CustomForm = () => {
 
     return (
-        <form className="form" onSubmit={formik.handleSubmit}>
-            <h2>Отправить пожертвование</h2>
-            <label htmlFor="name">Ваше имя</label>
-            <input
-                id="name"
-                name="name"
-                type="text"
-                value={formik.values.name}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur} // ушел фокус с элемента
-            />
-            {formik.errors.name && formik.touched.name ? <div className='error'>{formik.errors.name}</div> : null}
-            <label htmlFor="email">Ваша почта</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur} // ушел фокус с элемента
-            />
-            {formik.errors.email && formik.touched.email ? <div className='error'>{formik.errors.email}</div> : null}    
-            <label htmlFor="amount">Количество</label>
-            <input
-                id="amount"
-                name="amount"
-                type="number"
-                value={formik.values.amount}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-            />
-            {formik.errors.amount && formik.touched.amount ? <div className='error'>{formik.errors.amount}</div> : null}
-            <label htmlFor="currency">Валюта</label>
-            <select
-                id="currency"
-                name="currency"
-                value={formik.values.currency}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}>
-                    <option value="">Выберите валюту</option>
-                    <option value="USD">USD</option>
-                    <option value="UAH">UAH</option>
-                    <option value="RUB">RUB</option>
-            </select>
-            {formik.errors.currency && formik.touched.currency ? <div className='error'>{formik.errors.currency}</div> : null}
-            <label htmlFor="text">Ваше сообщение</label>
-            <textarea 
-                id="text"
-                name="text"
-                value={formik.values.text}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-            />
-            {formik.errors.text && formik.touched.text ? <div className='error'>{formik.errors.text}</div> : null}
-            <label className="checkbox">
-                <input name="terms" type="checkbox" 
-                value={formik.values.checkbox}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
+        <Formik
+            initialValues = {
+                {
+                    name: '',
+                    email: '',
+                    amount: 0,
+                    currency: '',
+                    text: '',
+                    terms: '',
+                }}
+            validationSchema = {Yup.object({
+                    name: Yup.string()
+                            .min(2, "Минимум 2 символа")
+                            .required('Поле обязательное'),
+                    email: Yup.string()
+                            .email("введите корректный Email")
+                            .required('Поле обязательное'),
+                    amount: Yup.number()
+                                .min(5, "Не менее 5")
+                                .required('Поле обязательное'),
+                    currency: Yup.string().required('Выберете валюту'),
+                    text: Yup.string().min(10, 'Не менее 10 символов'),
+                    terms: Yup.boolean()
+                                .oneOf([true], 'Необходимо согласие')
+                                .required('Необходимо согласие')            
+            })}
+
+            onSubmit = {values => console.log(JSON.stringify(values, null, 2))}
+        >
+            <Form className="form">
+                <h2>Отправить пожертвование</h2>
+                <label htmlFor="name">Ваше имя</label>
+                <Field
+                    id="name"
+                    name="name"
+                    type="text"                   
                 />
-                Соглашаетесь с политикой конфиденциальности?
-                {formik.errors.terms && formik.touched.terms ? <div className='error'>{formik.errors.terms}</div> : null}
-            </label>
-            <button type="submit">Отправить</button>
-        </form>
+                <ErrorMessage className='error' name='name' component={'div'}/>
+                <label htmlFor="email">Ваша почта</label>
+                <Field
+                    id="email"
+                    name="email"
+                    type="email"
+                />
+                <ErrorMessage className='error' name='email' component={'div'}/>
+                <label htmlFor="amount">Количество</label>
+                <Field
+                    id="amount"
+                    name="amount"
+                    type="number"            
+                />
+                <ErrorMessage className='error' name='amount' component={'div'}/>
+                <label htmlFor="currency">Валюта</label>
+                <Field
+                    id="currency"
+                    name="currency"
+                    as="select"
+                    >
+                        <option value="">Выберите валюту</option>
+                        <option value="USD">USD</option>
+                        <option value="UAH">UAH</option>
+                        <option value="RUB">RUB</option>
+                </Field>
+                <ErrorMessage className='error' name='currency' component={'div'}/>
+                <label htmlFor="text">Ваше сообщение</label>
+                <Field 
+                    id="text"
+                    name="text"
+                    as='textarea'
+                />
+                <ErrorMessage className='error' name='text' component={'div'}/>
+                <label className="checkbox">
+                    <Field name="terms" type="checkbox" 
+                   
+                    />
+                    Соглашаетесь с политикой конфиденциальности?
+                    <ErrorMessage className='error' name='terms' component={'div'}/>   
+                </label>
+                <button type="submit">Отправить</button>
+            </Form>
+        </Formik>
     )
 }
 
-export default Form;
+export default CustomForm;
